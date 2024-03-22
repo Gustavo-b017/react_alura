5819adb9a4f6b64ffb32fbd06f2578487a11ddf0
Gustavo-b017 <gustavo.estudo017@gmail.com>
2024-03-21 21:14:19 -0300

    comecando a brincabeira hihihhih

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './img/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p><strong>Alura</strong> books</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
