b7256e8a0aea27f6096b71175bb5cd1206517947
Gustavo-b017 <gustavo.estudo017@gmail.com>
2024-03-27 09:26:52 -0300

    f

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import logo from './img/logo.svg';
 import './App.css';
+import Logo from './componentes/Logo/Logo';
+import perfil from './img/perfil.svg'
+import sacola from './img/sacola.svg'
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icones = [perfil, sacola]
 
 function App() {
-  return (
-    <div className="app">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p><strong>Alura</strong> books</p>
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <Logo></Logo>
+                <ul className='opcoes'>
+                    { textoOpcoes.map ( (texto) => (
+                        <li className='opcao'><p>{texto}</p></li>
+                    ) ) }
+                </ul>
+
+        <ul className='icones'>
+            { icones.map( (icone) => (
+              <li className='icone'><img src={icone}></img></li>
+            )) }
+        </ul>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
